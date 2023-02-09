9302f0ae1909905c747b6cf0ec855c5b5306cbf3
callazar <callazar1234@gmail.com>
2023-02-09 14:23:24 +0700

    update, feb 9, 14:23

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,16 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
+    <img class="w-full bg-cover" src="./img/P3.jpg" alt="">
     <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+      <nav class="absolute inset-y-0 w-full h-12 p-3  backdrop-blur-sm bg-white/30">
+        <RouterLink class="px-2 font-semibold text-white uppercase hover:text-slate-300" to="/">Home</RouterLink>
+        <RouterLink class="px-2 font-semibold text-white uppercase hover:text-slate-300" to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
 
-  <RouterView />
+  <!-- <RouterView /> -->
 </template>
 
 
