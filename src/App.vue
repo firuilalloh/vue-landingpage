3c69a8dbef97bfa8aadc8f5f8b7d27ad01723eb2
callazar <callazar1234@gmail.com>
2023-02-10 14:41:35 +0700

    update, feb 10, 14:41

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,14 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <img class="w-full bg-cover" src="./img/P3.jpg" alt="">
+    <img class="fixed w-full bg-cover" src="./img/P3.jpg" alt="">
     <div>
-      <nav class="absolute inset-y-0 w-full h-12 p-3  backdrop-blur-sm bg-white/30">
-        <RouterLink class="px-2 font-semibold text-white uppercase hover:text-slate-300" to="/">Home</RouterLink>
-        <RouterLink class="px-2 font-semibold text-white uppercase hover:text-slate-300" to="/about">About</RouterLink>
+      <nav class="absolute inset-y-0 w-full h-12 p-3 backdrop-blur-sm bg-white/30">
+        <RouterLink class="px-2 font-semibold text-white uppercase hover:text-slate-300 hover:underline underline-offset-4 decoration-2" to="/">Home</RouterLink>
+        <RouterLink class="px-2 font-semibold text-white uppercase hover:text-slate-300 hover:underline underline-offset-4 decoration-2" to="/team">Team</RouterLink>
+        <RouterLink class="px-2 font-semibold text-white uppercase hover:text-slate-300 hover:underline underline-offset-4 decoration-2" to="/project">Project</RouterLink>
+        <RouterLink class="px-2 font-semibold text-white uppercase hover:text-slate-300 hover:underline underline-offset-4 decoration-2" to="/about">About</RouterLink>
+        <RouterLink class="px-2 font-semibold text-white uppercase hover:text-slate-300 hover:underline underline-offset-4 decoration-2" to="/contact">contact</RouterLink>
       </nav>
     </div>
   </header>
