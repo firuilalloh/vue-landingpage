df15dda3be645b9fb3c9543dd035ec84ccce2f30
callazar <callazar1234@gmail.com>
2023-02-15 16:16:36 +0700

    update, feb 15, 16:16

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,19 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <img class="fixed w-full bg-cover" src="./img/P3.jpg" alt="">
     <div>
-      <nav class="absolute inset-y-0 w-full h-12 p-3 backdrop-blur-sm bg-white/30">
+      <nav class="absolute inset-y-0 w-full h-12 p-3 backdrop-blur-sm">
         <RouterLink class="px-2 font-semibold text-white uppercase hover:text-slate-300 hover:underline underline-offset-4 decoration-2" to="/">Home</RouterLink>
         <RouterLink class="px-2 font-semibold text-white uppercase hover:text-slate-300 hover:underline underline-offset-4 decoration-2" to="/team">Team</RouterLink>
         <RouterLink class="px-2 font-semibold text-white uppercase hover:text-slate-300 hover:underline underline-offset-4 decoration-2" to="/project">Project</RouterLink>
         <RouterLink class="px-2 font-semibold text-white uppercase hover:text-slate-300 hover:underline underline-offset-4 decoration-2" to="/about">About</RouterLink>
         <RouterLink class="px-2 font-semibold text-white uppercase hover:text-slate-300 hover:underline underline-offset-4 decoration-2" to="/contact">contact</RouterLink>
       </nav>
+      <RouterLink class="absolute h-10 px-4 py-1.5 font-semibold text-white rounded-full right-2 border border-white inset-y-1 hover:scale-105 duration-300" to="/login">Register</RouterLink>
+      <RouterLink class="absolute h-10 px-4 py-1.5 font-semibold text-white rounded-full right-28  inset-y-1 hover:backdrop-blur-md duration-300" to="/signin">Sign In</RouterLink>
     </div>
   </header>
 
-  <!-- <RouterView /> -->
+  <RouterView />
 </template>
 
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <p>TEST VUE</p>
+  <main class="absolute p-4 rounded-md left-80 inset-y-52">
+    <div
+      class="font-semibold tracking-wider text-white uppercase font-montserrat"
+    >
+    <img src="" alt="">
+      <h1 class="mb-2 text-4xl">welcome to my</h1>
+      <h3 class="text-xl tracking-widest underline underline-offset-4">
+        portofolio website
+      </h3>
+    </div>
+    <p
+      class="pt-4 font-medium text-justify text-white uppercase w-96 font-montserrat"
+    >
+      Hello, my name is fafirru ilalloh i'm a gamer, programer and youtuber. If
+      you wonto subscribe my youtube channel, you can klick this button in
+      bellow
+    </p>
+    <button
+      class="px-2 py-2 mt-3 font-medium text-white uppercase duration-300 border border-white rounded-full hover:scale-105 font-montserrat "
+    >
+      my channel youtube
+    </button>
   </main>
 </template>
